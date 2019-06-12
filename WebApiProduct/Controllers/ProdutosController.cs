ebc848da82c8bb0f00f06d1e068bdd15b1d16469
Crystyano Almeida <crystyano.pereira@prolins.com.br>
2019-06-12 16:59:56 -0300

    Send Code

---
+++ WebApiProduct/Controllers/ProdutosController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiProduct.Model;
+
+namespace WebApiProduct.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public ProdutosController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/produtos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produto>>> GetProdutos()
+        {
+            return await _context.Produtos.ToListAsync();
+        }
+
+        // GET: api/produtos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produto>> GetProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            return produto;
+        }
+
+        // PUT: api/produtos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduto(int id, Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProdutoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/produtos
+        [HttpPost]
+        public async Task<ActionResult<Produto>> PostProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduto", new { id = produto.Id }, produto);
+        }
+
+        // DELETE: api/produtos/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Produto>> DeleteProduto(int id)
+        {
+            var produto = await _context.Produtos.FindAsync(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+
+            return produto;
+        }
+
+        private bool ProdutoExists(int id)
+        {
+            return _context.Produtos.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebApiProduct/Model/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProduct.Model
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+
+        public string Descricao { get; set; }
+
+        public double preco { get; set; }
+
+        public int quantidade { get; set; }
+
+        public bool status { get; set; }
+
+    }
+}
